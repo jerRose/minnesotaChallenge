fead7002094ba33d0cc5b7719def4e7d0bd1c56f
jerRose <64826662+jerRose@users.noreply.github.com>
2023-02-28 17:42:39 -0500

    grid search to fit logistic function

--- teamSuccess.R
+++ teamSuccess.R
@@ -1,4 +1,8 @@
 ##expected wins
+plan(multisession)
+require(furrr)
+require(rgl)
+
 logisticFunction <- function(maxVal = 1, middleVal, steepness, x){
   y <- maxVal / (1 + exp(-steepness * (x - middleVal)))
   return(y)
@@ -50,13 +54,41 @@ fullTeamPerformance <- elo538wInit %>%
   rename('gameNum1' = 'gameNum.x',
          'gameNum2' = 'gameNum.y') %>%
   left_join(., elo538 %>% select(date, team1, team2, elo1_pre, elo2_pre, score1, score2), by = c('date' = 'date', 'team1' = 'team1', 'team2'='team2','elo1_pre' = 'elo1_pre', 'elo2_pre' = 'elo2_pre')) %>%
-  mutate(teamPerfLogistic = eloDiff1 * logisticFunction(middleVal = 50, steepness = .05, x = gameNum2),
+  mutate(teamPerfLogistic = eloDiff1 * logisticFunction(middleVal = 91, steepness = .075, x = gameNum2),
          date = as.numeric(gsub('-','',date))) %>%
-  inner_join(.,gameLogs %>% select(Date, Attendance, HomeTeam, VisitingTeam, VistingTeamScore, HomeTeamScore), 
+  inner_join(.,gameLogs %>% select(Date, Attendance, HomeTeam, VisitingTeam, VistingTeamScore, HomeTeamScore, DayofWeek), 
             by = c('date' = 'Date', 'team1' = 'HomeTeam', 'team2' = 'VisitingTeam', 'score1' = 'HomeTeamScore', 'score2' = 'VistingTeamScore')) %>% ###14 DUPES PLEASE GO BACK AND CHECK LATER (CAUSED BY DOUBLE HEADERS W SAME SCORE) -- wait maybe not but still weird
   group_by(team1, season) %>%
   mutate(avgAtt = mean(Attendance)) %>%
   ungroup() %>%
-  mutate(diffFromAvgAtt = Attendance - avgAtt)
+  mutate(diffFromAvgAtt = Attendance - avgAtt,
+         weekend = ifelse(DayofWeek %in% c('Fri','Sat','Sun'), 1, 0))
 
-ggplot(fullTeamPerformance, aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point() + geom_smooth(method = 'lm', se = F)
+ggplot(fullTeamPerformance, aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point(aes(color = as.factor(weekend))) + geom_smooth(method = 'lm', se = F, aes(fill = as.factor(weekend))) + geom_smooth(method = 'lm', se = F, color = 'red')
+
+cor(fullTeamPerformance[fullTeamPerformance$weekend == 0,]$teamPerfLogistic, fullTeamPerformance[fullTeamPerformance$weekend == 0,]$diffFromAvgAtt, use="complete.obs")
+
+
+
+measureLogistic <- function(midVal, steepVal){
+  ft <- fullTeamPerformance %>%
+    mutate(teamPerfLogistic = eloDiff1 * logisticFunction(middleVal = midVal, steepness = steepVal, x = gameNum2))
+  
+  all <- cor(ft$teamPerfLogistic, ft$diffFromAvgAtt, use="complete.obs")
+  weekend <- cor(ft[ft$weekend == 1,]$teamPerfLogistic, ft[ft$weekend == 1,]$diffFromAvgAtt, use="complete.obs")
+  weekday <- cor(ft[ft$weekend == 0,]$teamPerfLogistic, ft[ft$weekend == 0,]$diffFromAvgAtt, use="complete.obs")
+  
+  finalMat <- matrix(c(midVal, steepVal, all, weekend, weekday), nrow = 1) %>% data.frame()
+  names(finalMat) <- c('midVal', 'steepVal', 'corAll', 'corWeekend', 'corWeekday')
+  return(finalMat)
+}
+
+midValVec <- seq.int(0, 162, by = .5)
+steepValVec <- seq.int(0, 1, by = .005)
+allCombs <- expand.grid(midValVec, steepValVec)
+
+corrMatrixLogistic <- future_map2_dfr(as.list(allCombs$Var1), as.list(allCombs$Var2), measureLogistic, .progress = T)
+plot3d(corrMatrixLogistic$midVal, corrMatrixLogistic$steepVal, corrMatrixLogistic$corWeekend)
+ggplot(fullTeamPerformance, aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point(aes(color = as.factor(weekend))) + geom_smooth(se = F, aes(fill = as.factor(weekend))) + geom_smooth(method = 'lm', se = F, color = 'red')
+ggplot(fullTeamPerformance[fullTeamPerformance$weekend == 1,], aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point(aes(color = as.factor(weekend))) + geom_smooth(se = F, aes(fill = as.factor(weekend))) + geom_smooth(method = 'lm', se = F, color = 'red')
+ggplot(fullTeamPerformance[fullTeamPerformance$weekend == 0,], aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point(aes(color = as.factor(weekend))) + geom_smooth(se = F, aes(fill = as.factor(weekend))) + geom_smooth(method = 'lm', se = F, color = 'red')
