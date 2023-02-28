22a79aa1a5f766756c463e99de1579d59db47abe
jerRose <64826662+jerRose@users.noreply.github.com>
2023-02-27 23:37:35 -0500

    made logistic function for attendance and merged attendance data with 538 (also made team name conversion function btwn 538 data and given data)

--- readInData.R
+++ readInData.R
@@ -1,9 +1,28 @@
 require(tidyverse)
 require(data.table)
 require(stringr)
+require(stringi)
+
+convert538Teams <- function(df538){
+  numVec <- sapply(df538, class) %>% sapply(function(x) x[1])  %>% unlist() %>% unname() %in% c('integer', 'numeric')
+  
+  newNames <- lapply(df538, function(x) stri_replace_all_regex(x,
+                                                         pattern = c('NYY','SDP','LAD','NYM','STL','CHC','CHW','FLA','KCR','SFG','TBD','WSN'),
+                                                         replacement = c('NYA','SDN','LAN','NYN','SLN','CHN','CHA','FLO','KCA','SFN','TBA','WAS'),
+                                                         vectorize = F)) %>%
+    data.frame()
+  
+  newNames[,numVec] <- apply(newNames[,numVec], 2, function(x) as.numeric(x))
+  
+  return(newNames)  
+}
 
 mlbSchedule2023 <- fread('data/2023_MLBSchedule.csv')
 originalSchedules <- read.csv('data/OriginalSchedules.csv', fileEncoding = 'UTF-16')
 gameLogs <- fread('data/smallGameLogs.csv') %>%
   select(-V1)
-elo538 <- fread('data/mlb_elo.csv')
+elo538 <- fread('data/mlb_elo.csv') %>%
+  convert538Teams()
+
+
+
--- teamSuccess.R
+++ teamSuccess.R
@@ -28,10 +28,35 @@ elo538wInit <- elo538Small %>%
   rename('elo1PreSeason' = 'elo.x',
          'elo2PreSeason' = 'elo.y') %>%
   mutate(eloDiff1 = elo1_pre - elo1PreSeason,
-         eloDiff2 = elo2_pre - elo2PreSeason) 
+         eloDiff2 = elo2_pre - elo2PreSeason)
 
+gameNum <- elo538Small %>%
+  select(date, team1, season, elo1_pre, rowN) %>%
+  rename('team' = 'team1',
+         'elo' = 'elo1_pre') %>% 
+  rbind(elo538Small %>%
+          select(date, team2, season, elo2_pre, rowN) %>%
+          rename('team' = 'team2','elo' = 'elo2_pre')) %>%
+  arrange(desc(rowN)) %>%
+  group_by(season, team) %>%
+  mutate(gameNum = 1:n()) %>%
+  mutate(Date = gsub('-','', date)) %>%
+  ungroup() %>%
+  select(Date, date, team, gameNum, elo)
 
+fullTeamPerformance <- elo538wInit %>%
+  left_join(.,gameNum %>% select(date, gameNum, team, elo), by = c('date' = 'date', 'team1' = 'team', 'elo1_pre' = 'elo')) %>%
+  left_join(., gameNum %>% select(date, gameNum, team, elo), by = c('date' = 'date', 'team2' = 'team', 'elo2_pre' = 'elo')) %>%
+  rename('gameNum1' = 'gameNum.x',
+         'gameNum2' = 'gameNum.y') %>%
+  left_join(., elo538 %>% select(date, team1, team2, elo1_pre, elo2_pre, score1, score2), by = c('date' = 'date', 'team1' = 'team1', 'team2'='team2','elo1_pre' = 'elo1_pre', 'elo2_pre' = 'elo2_pre')) %>%
+  mutate(teamPerfLogistic = eloDiff1 * logisticFunction(middleVal = 50, steepness = .05, x = gameNum2),
+         date = as.numeric(gsub('-','',date))) %>%
+  inner_join(.,gameLogs %>% select(Date, Attendance, HomeTeam, VisitingTeam, VistingTeamScore, HomeTeamScore), 
+            by = c('date' = 'Date', 'team1' = 'HomeTeam', 'team2' = 'VisitingTeam', 'score1' = 'HomeTeamScore', 'score2' = 'VistingTeamScore')) %>% ###14 DUPES PLEASE GO BACK AND CHECK LATER (CAUSED BY DOUBLE HEADERS W SAME SCORE) -- wait maybe not but still weird
+  group_by(team1, season) %>%
+  mutate(avgAtt = mean(Attendance)) %>%
+  ungroup() %>%
+  mutate(diffFromAvgAtt = Attendance - avgAtt)
 
-
-
-
+ggplot(fullTeamPerformance, aes(teamPerfLogistic, diffFromAvgAtt)) + geom_point() + geom_smooth(method = 'lm', se = F)
